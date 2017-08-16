64eabf1b96e514e261365b38d565afdfb3557300
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-15 19:07:58 -0500

    initial commit for event listeners

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+console.log("main.js");
+
+let Hadlebars = require('hbsfy/runtime');
+let tpData = require('../database.js');
+let tpEventList = require('../eventlisteners.js');
+    //newVarTemplate = require(filepath),
+    //newerVarTemplate = require(filepath);
+
