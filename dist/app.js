7b6f438622daff4d202a811b9f7f96fb1cfe26d8
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-16 15:38:46 -0500

    This will be the new master... Doenst look pretty, but it works

--- dist/app.js
+++ dist/app.js
@@ -1383,7 +1383,7 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
 
   return "\r\n    <div id=\"area--"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\"  style=\"background-color: #"
+    + "\" class=\"col-sm-4\"  style=\"background-color: #"
     + alias4(((helper = (helper = helpers.colorTheme || (depth0 != null ? depth0.colorTheme : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"colorTheme","hash":{},"data":data}) : helper)))
     + "\">\r\n\r\n        <h3 class=\"\">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
@@ -1391,8 +1391,7 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<img src=\"images/map.jpg\" class=\"img-responsive\" width=\"600\" height=\"700\">\r\n\r\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 
 },{"hbsfy/runtime":23}],25:[function(require,module,exports){
