34b963b6fac2fc1d77f8a8e4dd89325d5aa44185
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-08-16 11:18:58 -0500

    newest

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let Handlebars = require('hbsfy/runtime'),
-tpData = require('./javascripts/database.js'),
-tpEventList = require('./javascripts/eventlisteners.js'),
+tpData = require('./database.js'),
+tpEventList = require('./eventlistener.js'),
 welcomeTemplate = require('../templates/welcome.hbs'),
 welcomeData = require('../templates/welcome-data.js');
 
